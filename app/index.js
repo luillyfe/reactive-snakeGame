76c5c185ab714a44ca9f19406072c1ef7365c80b
Fermin Blanco <luillyfe89@gmail.com>
2020-07-29 15:15:51 -0500

    feature: basic game layout;

---
+++ app/index.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+context.fillStyle = 'white';
+context.fillRect(16, 16 , 16, 16);
+
+
+context.fillStyle = 'gray';
+context.fillRect(160, 160 , 16, 16);
