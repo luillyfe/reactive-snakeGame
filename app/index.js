8625ad51db19d6239cc9ee380ba738b59c6f7cf7
Fermin Blanco <luillyfe89@gmail.com>
2020-08-11 06:01:58 -0500

    feature: draw game on canvas context.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
-console.log('I am working');
+import { game$ } from '/store/index.js'
+
+let canvas = document.getElementById('canvas')
+let context = canvas.getContext('2d')
+
+initGame()
+
+
+function initGame() {
+    game$
+        .subscribe(drawGame)
+}
+
+function drawGame({snake, food, tile}) {
+    draw(snake.color, snake.position, tile)
+    draw(food.color, food.position, tile)
+}
+
+function draw(color, position, size) {
+    context.fillStyle = color
+    position.forEach(({x, y}) => {
+        context.fillRect(x, y, size, size)
+    })
+}
